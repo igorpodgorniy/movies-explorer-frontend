1ff73494912f65464c7304502d6dca0b88e9bbef
Igor Podgorniy <ipod-92@mail.ru>
2022-10-29 19:31:30 +0300

    Убрать лишний nav и сделать ссылки через список

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,22 +2,22 @@ import './Header.css';
 
 function Header() {
   return (
-    <header class="header">
-      <a href="#" class="header__logo"></a>
-      <nav class="nav">
-        <ul class="nav__list">
-          <li class="nav__item"><a href="#" class="nav__link nav__link_active">Фильмы</a></li>
-          <li class="nav__item"><a href="#" class="nav__link">Сохранённые фильмы</a></li>
+    <header className="header">
+      <a href="#" className="header__logo"></a>
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item"><a href="#" className="nav__link nav__link_active">Фильмы</a></li>
+          <li className="nav__item"><a href="#" className="nav__link">Сохранённые фильмы</a></li>
         </ul>
       </nav>
-      <button type="button" class="header__button">
-        <span class="header__button-icon"></span>
+      <button type="button" className="header__button">
+        <span className="header__button-icon"></span>
         Аккаунт
       </button>
-      <nav class="header__user-nav">
-        <a href="#" class="header__link">Регистрация</a>
-        <a href="#" class="header__link header__link_dark">Войти</a>
-      </nav>
+      <ul className="header__user-nav">
+        <li><a href="#" className="header__link">Регистрация</a></li>
+        <li><a href="#" className="header__link header__link_dark">Войти</a></li>
+      </ul>
     </header>
   )
 }
