18e6fb613deb5bab3494c20befe8dd02ccda08bc
Igor Podgorniy <ipod-92@mail.ru>
2022-11-01 21:55:52 +0300

    Сверстать десктопную версию страницы 404

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,10 +4,12 @@ import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import NotFound from '../NotFound/NotFound';
 
 function App() {
   return (
     <>
+      <NotFound></NotFound>
       <Header></Header>
       <SearchForm></SearchForm>
       <MoviesCardList></MoviesCardList>
