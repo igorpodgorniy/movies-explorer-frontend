825cd650d5bab0ccedb1a9f60f2d47255ec104c0
Igor Podgorniy <ipod-92@mail.ru>
2022-11-01 20:38:06 +0300

    Сверстать десктопную версию блока с кнопкой Ещё

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -3,128 +3,133 @@ import photo from '../../images/film-frame.png';
 
 function MoviesCardList() {
   return (
-    <ul className="films">
-      <li>
-        <article className="film">
-          <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
-          <div className="film__info">
-            <h2 className="film__name">33 слова о дизайне</h2>
-            <p className="film__duration">1ч42м</p>
-            <button className="film__favorite" type="button" aria-label="Сохранить фильм"></button>
-          </div>
-        </article>
-      </li>
-      <li>
-        <article className="film">
-          <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
-          <div className="film__info">
-            <h2 className="film__name">33 слова о дизайне</h2>
-            <p className="film__duration">1ч42м</p>
-            <button className="film__favorite film__favorite_active" type="button" aria-label="Сохранить фильм"></button>
-          </div>
-        </article>
-      </li>
-      <li>
-        <article className="film">
-          <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
-          <div className="film__info">
-            <h2 className="film__name">33 слова о дизайне</h2>
-            <p className="film__duration">1ч42м</p>
-            <button className="film__favorite" type="button" aria-label="Сохранить фильм"></button>
-          </div>
-        </article>
-      </li>
-      <li>
-        <article className="film">
-          <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
-          <div className="film__info">
-            <h2 className="film__name">33 слова о дизайне</h2>
-            <p className="film__duration">1ч42м</p>
-            <button className="film__favorite film__favorite_active" type="button" aria-label="Сохранить фильм"></button>
-          </div>
-        </article>
-      </li>
-      <li>
-        <article className="film">
-          <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
-          <div className="film__info">
-            <h2 className="film__name">33 слова о дизайне</h2>
-            <p className="film__duration">1ч42м</p>
-            <button className="film__favorite" type="button" aria-label="Сохранить фильм"></button>
-          </div>
-        </article>
-      </li>
-      <li>
-        <article className="film">
-          <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
-          <div className="film__info">
-            <h2 className="film__name">33 слова о дизайне</h2>
-            <p className="film__duration">1ч42м</p>
-            <button className="film__favorite film__favorite_active" type="button" aria-label="Сохранить фильм"></button>
-          </div>
-        </article>
-      </li>
-      <li>
-        <article className="film">
-          <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
-          <div className="film__info">
-            <h2 className="film__name">33 слова о дизайне</h2>
-            <p className="film__duration">1ч42м</p>
-            <button className="film__favorite" type="button" aria-label="Сохранить фильм"></button>
-          </div>
-        </article>
-      </li>
-      <li>
-        <article className="film">
-          <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
-          <div className="film__info">
-            <h2 className="film__name">33 слова о дизайне</h2>
-            <p className="film__duration">1ч42м</p>
-            <button className="film__favorite film__favorite_active" type="button" aria-label="Сохранить фильм"></button>
-          </div>
-        </article>
-      </li>
-      <li>
-        <article className="film">
-          <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
-          <div className="film__info">
-            <h2 className="film__name">33 слова о дизайне</h2>
-            <p className="film__duration">1ч42м</p>
-            <button className="film__favorite" type="button" aria-label="Сохранить фильм"></button>
-          </div>
-        </article>
-      </li>
-      <li>
-        <article className="film">
-          <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
-          <div className="film__info">
-            <h2 className="film__name">33 слова о дизайне</h2>
-            <p className="film__duration">1ч42м</p>
-            <button className="film__favorite film__favorite_active" type="button" aria-label="Сохранить фильм"></button>
-          </div>
-        </article>
-      </li>
-      <li>
-        <article className="film">
-          <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
-          <div className="film__info">
-            <h2 className="film__name">33 слова о дизайне</h2>
-            <p className="film__duration">1ч42м</p>
-            <button className="film__favorite" type="button" aria-label="Сохранить фильм"></button>
-          </div>
-        </article>
-      </li>
-      <li>
-        <article className="film">
-          <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
-          <div className="film__info">
-            <h2 className="film__name">33 слова о дизайне</h2>
-            <p className="film__duration">1ч42м</p>
-            <button className="film__favorite film__favorite_active" type="button" aria-label="Сохранить фильм"></button>
-          </div>
-        </article>
-      </li>
-    </ul>
+    <>
+      <ul className="films">
+        <li>
+          <article className="film">
+            <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
+            <div className="film__info">
+              <h2 className="film__name">33 слова о дизайне</h2>
+              <p className="film__duration">1ч42м</p>
+              <button className="film__favorite" type="button" aria-label="Сохранить фильм"></button>
+            </div>
+          </article>
+        </li>
+        <li>
+          <article className="film">
+            <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
+            <div className="film__info">
+              <h2 className="film__name">33 слова о дизайне</h2>
+              <p className="film__duration">1ч42м</p>
+              <button className="film__favorite film__favorite_active" type="button" aria-label="Сохранить фильм"></button>
+            </div>
+          </article>
+        </li>
+        <li>
+          <article className="film">
+            <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
+            <div className="film__info">
+              <h2 className="film__name">33 слова о дизайне</h2>
+              <p className="film__duration">1ч42м</p>
+              <button className="film__favorite" type="button" aria-label="Сохранить фильм"></button>
+            </div>
+          </article>
+        </li>
+        <li>
+          <article className="film">
+            <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
+            <div className="film__info">
+              <h2 className="film__name">33 слова о дизайне</h2>
+              <p className="film__duration">1ч42м</p>
+              <button className="film__favorite film__favorite_active" type="button" aria-label="Сохранить фильм"></button>
+            </div>
+          </article>
+        </li>
+        <li>
+          <article className="film">
+            <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
+            <div className="film__info">
+              <h2 className="film__name">33 слова о дизайне</h2>
+              <p className="film__duration">1ч42м</p>
+              <button className="film__favorite" type="button" aria-label="Сохранить фильм"></button>
+            </div>
+          </article>
+        </li>
+        <li>
+          <article className="film">
+            <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
+            <div className="film__info">
+              <h2 className="film__name">33 слова о дизайне</h2>
+              <p className="film__duration">1ч42м</p>
+              <button className="film__favorite film__favorite_active" type="button" aria-label="Сохранить фильм"></button>
+            </div>
+          </article>
+        </li>
+        <li>
+          <article className="film">
+            <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
+            <div className="film__info">
+              <h2 className="film__name">33 слова о дизайне</h2>
+              <p className="film__duration">1ч42м</p>
+              <button className="film__favorite" type="button" aria-label="Сохранить фильм"></button>
+            </div>
+          </article>
+        </li>
+        <li>
+          <article className="film">
+            <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
+            <div className="film__info">
+              <h2 className="film__name">33 слова о дизайне</h2>
+              <p className="film__duration">1ч42м</p>
+              <button className="film__favorite film__favorite_active" type="button" aria-label="Сохранить фильм"></button>
+            </div>
+          </article>
+        </li>
+        <li>
+          <article className="film">
+            <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
+            <div className="film__info">
+              <h2 className="film__name">33 слова о дизайне</h2>
+              <p className="film__duration">1ч42м</p>
+              <button className="film__favorite" type="button" aria-label="Сохранить фильм"></button>
+            </div>
+          </article>
+        </li>
+        <li>
+          <article className="film">
+            <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
+            <div className="film__info">
+              <h2 className="film__name">33 слова о дизайне</h2>
+              <p className="film__duration">1ч42м</p>
+              <button className="film__favorite film__favorite_active" type="button" aria-label="Сохранить фильм"></button>
+            </div>
+          </article>
+        </li>
+        <li>
+          <article className="film">
+            <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
+            <div className="film__info">
+              <h2 className="film__name">33 слова о дизайне</h2>
+              <p className="film__duration">1ч42м</p>
+              <button className="film__favorite" type="button" aria-label="Сохранить фильм"></button>
+            </div>
+          </article>
+        </li>
+        <li>
+          <article className="film">
+            <img className="film__img" src={photo} alt="Кадр из фильма '33 слова о дизайне'" />
+            <div className="film__info">
+              <h2 className="film__name">33 слова о дизайне</h2>
+              <p className="film__duration">1ч42м</p>
+              <button className="film__favorite film__favorite_active" type="button" aria-label="Сохранить фильм"></button>
+            </div>
+          </article>
+        </li>
+      </ul>
+      <section className="more">
+        <button className="more__button" type="button" aria-label="Подгрузить ещё фильмы">Ещё</button>
+      </section>
+    </>
   );
 }
 
