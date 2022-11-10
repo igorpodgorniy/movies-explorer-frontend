991103162983a3e0cc036b6d1ff667b7f5d05b43
Igor Podgorniy <ipod-92@mail.ru>
2022-11-10 13:18:54 +0300

    Сверстать выезжающее меню для планшета и мобильных

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,28 +4,41 @@ import { NavLink, useHistory } from 'react-router-dom';
 function Navigation(props) {
   const history = useHistory();
   const { isLogin } = props;
+  const navClassName = `nav ${history.location.pathname !== '/' ? 'nav_without-profile' : ''}`;
   return (
     <>
-      {(isLogin || history.location.pathname === '/')&&
-        <nav className="nav">
-          <ul className="nav__list">
-            <li className="nav__item"><NavLink to="/movies" activeClassName='nav__link_active' className="nav__link">Фильмы</NavLink></li>
-            <li className="nav__item"><NavLink to="/saved-movies" activeClassName='nav__link_active' className="nav__link">Сохранённые фильмы</NavLink></li>
-          </ul>
-        </nav>
-      }
       {(history.location.pathname === '/movies' || history.location.pathname === '/saved-movies' || history.location.pathname === '/profile') &&
         <>
-          <button type="button" className="header__button" onClick={() => history.push('/profile')}>
-            <span className="header__button-icon"></span>
-            Аккаунт
-          </button>
-          <div className="header__hamburger">
-            <div className="header__nav-bar header__nav-bar_position_top"></div>
-            <div className="header__nav-bar header__nav-bar_position_middle"></div>
-            <div className="header__nav-bar header__nav-bar_position_bottom"></div>
-          </div>
+          <input type="checkbox" id="ticker" className="nav__hamburger-ticker"></input>
+          <label for="ticker" className="nav__hamburger">
+            <div className="nav__nav-bar nav__nav-bar_position_top"></div>
+            <div className="nav__nav-bar nav__nav-bar_position_middle"></div>
+            <div className="nav__nav-bar nav__nav-bar_position_bottom"></div>
+          </label>
         </>
+      }
+      {(isLogin || history.location.pathname === '/') &&
+        <div className="nav__container">
+          <nav className={navClassName}>
+            <ul className="nav__list">
+              <li className="nav__item nav__item_hidden">
+                <NavLink exact to="/" activeClassName='nav__link_active' className="nav__link nav__item_hidden">Главная</NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink to="/movies" activeClassName='nav__link_active' className="nav__link">Фильмы</NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink to="/saved-movies" activeClassName='nav__link_active' className="nav__link">Сохранённые фильмы</NavLink>
+              </li>
+            </ul>
+            {history.location.pathname !== '/' &&
+              <button type="button" className="nav__button" onClick={() => history.push('/profile')}>
+                <span className="nav__button-icon"></span>
+                Аккаунт
+              </button>
+            }
+          </nav>
+        </div>
       }
     </>
   );
