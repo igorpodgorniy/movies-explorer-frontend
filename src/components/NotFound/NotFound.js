0528f38290557a46f0aacbd32a343b543c3e524a
Igor Podgorniy <ipod-92@mail.ru>
2022-11-22 10:17:44 +0300

    Скорректировать поведение кнопки Назад на 404-странице

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,12 +1,18 @@
 import './NotFound.css';
-import { NavLink } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 function NotFound() {
+  const history = useHistory();
+
+  function handleBackButton() {
+    history.goBack()
+  }
+
   return (
     <main className="not-found">
       <h1 className="not-fount__header">404</h1>
       <p className="not-fount__text">Страница не найдена</p>
-      <NavLink to="/" className="not-found__link">Назад</NavLink>
+      <button onClick={handleBackButton} className="not-found__link">Назад</button>
     </main>
   );
 }
